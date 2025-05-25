8179dadfc61561b7615cf28791f4967e1c0c77a9
Egvenii Baliuk <ben_jr@bk.ru>
2025-05-25 23:18:31 +0300

    Task_4: add H2 database for tests

--- src/test/java/com/javarush/jira/BaseTests.java
+++ src/test/java/com/javarush/jira/BaseTests.java
@@ -2,8 +2,11 @@ package com.javarush.jira;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Sql(scripts = {"classpath:db/changelog-test.sql", "classpath:db/data-test.sql"}, config = @SqlConfig(encoding = "UTF-8"))
 abstract class BaseTests {
 }
