3cd8b00f5538f5d1a9c8cd8b10a8918197d4c57c
Egvenii Baliuk <ben_jr@bk.ru>
2025-05-26 00:05:48 +0300

    Task_5: add tests for ProfileRestController

--- README.md
+++ README.md
@@ -27,8 +27,22 @@
 <br>
 <p>&#10004;&#65039; 3. Transfer sensitive information (login, database password, identifiers for OAuth registration/authorization, mail settings) to a separate property file application-secrets.yaml. Put link for that file in .gitignore</p>
 <img src="resources/images/Task_03_1_AppSecret.png"/>
+<div align="center"><b>Application secret file</b></div>
 <br>
 <img src="resources/images/Task_03_2_ConfigProd.png"/>
+<div align="center"><b>Config prod profile</b></div>
 <br>
 <img src="resources/images/Task_03_3_ConfigTest.png"/>
+<div align="center"><b>Config test profile</b></div>
+<br>
+<p>&#10004;&#65039; 4. Correct the tests so that during the tests the in memory database (H2) is used, and not PostgreSQL. To do this, you need to define 2 beans, and the selection of which one to use should be determined by the active Spring profile.</p>
+<img src="resources/images/Task_04_1_AddDependency.png"/>
+<div align="center"><b>Add dependency</b></div>
+<br>
+<img src="resources/images/Task_04_2_RunningTests.png"/>
+<div align="center"><b>All Tests Running</b></div>
+<br>
+<p>&#10004;&#65039; 5. Write tests for all public methods of the ProfileRestController controller.</p>
+<img src="resources/images/Task_05_1_RunProfileRestControllerTests.png"/>
+<div align="center"><b>Run ProfileRestController tests</b></div>
 <br>
--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,121 @@
 package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
+import com.javarush.jira.common.BaseHandler;
+import com.javarush.jira.common.util.JsonUtil;
+import com.javarush.jira.profile.ProfileTo;
+import com.javarush.jira.profile.internal.ProfileRepository;
+import com.javarush.jira.profile.internal.model.Profile;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.javarush.jira.login.internal.web.UserTestData.*;
+import static com.javarush.jira.login.internal.web.UserTestData.USER_MAIL;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.*;
+import static com.javarush.jira.profile.internal.web.ProfileTestData.PROFILE_MATCHER;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 class ProfileRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL_PROFILE = BaseHandler.REST_URL + "/profile";
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getWithProfileCorrectDetails_andExpectSuccess() throws Exception {
+        ProfileTo profileTest = USER_PROFILE_TO;
+        profileTest.setId(USER_ID);
+        perform(MockMvcRequestBuilders.get(REST_URL_PROFILE))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(profileTest));
+    }
+
+
+
+    @Test
+    @WithUserDetails(GUEST_MAIL)
+    void getWithGuestDetails_andExpectThatOk() throws Exception {
+        ProfileTo profileTest = GUEST_PROFILE_EMPTY_TO;
+        profileTest.setId(GUEST_ID);
+        perform(MockMvcRequestBuilders.get(REST_URL_PROFILE))
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(PROFILE_TO_MATCHER.contentJson(GUEST_PROFILE_EMPTY_TO));
+
+    }
+
+    @Test
+    void getWithInvalidDetails_andExpectThatOk() throws Exception {
+        ProfileTo profileTest = ProfileTestData.getInvalidTo();
+        profileTest.setId(NOT_FOUND);
+        perform(MockMvcRequestBuilders.get(REST_URL_PROFILE).content(profileTest.toString()))
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized())
+        ;
+    }
+
+
+
+    // put
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateProfileValidDetails_andExpectOk() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL_PROFILE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getUpdatedTo())))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+
+        Profile dbProfile = profileRepository.getExisted(USER_ID);
+        Profile updatedProfileBefore = ProfileTestData.getUpdated(USER_ID);
+
+        PROFILE_MATCHER.assertMatch(dbProfile, updatedProfileBefore);
+    }
+
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateProfileInvalidProfile_andExpectIsUprocessableEntity() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL_PROFILE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getInvalidTo())))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateProfileInvalidDetailsThatIsUnknownContact() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL_PROFILE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getWithUnknownContactTo())))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void updateProfileInvalidDetailsThatIsHtmlUnsafeTest() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL_PROFILE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getWithContactHtmlUnsafeTo())))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
 
 }
